b6099428734a1344498b8bf677e59589e1ba4259
Raul Ugarte <raulugarte@Rauls-MBP-3.fritz.box>
2023-11-18 00:44:21 +0100

    hugo boss

--- blocks/cards2/cards2.js
+++ blocks/cards2/cards2.js
@@ -1,4 +1,11 @@
 
 export default function decorate(block) {
 
+    const row = block.firstElementChild;
+    const bg = row.querySelector('picture');
+    const bgP = bg.closest('p');
+    block.append(bg);
+    if (bgP) bgP.remove();
+    row.classList.add('cards-card-body');
+
 }
