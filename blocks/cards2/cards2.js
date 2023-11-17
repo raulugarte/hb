f7c8a194e8a203698327aaf8508c41875b9e57b1
Raul Ugarte <raulugarte@Rauls-MBP-3.fritz.box>
2023-11-18 00:54:50 +0100

    hugo boss

--- blocks/cards2/cards2.js
+++ blocks/cards2/cards2.js
@@ -1,11 +1,51 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
 
-    const row = block.firstElementChild;
-    const bg = row.querySelector('picture');
-    const bgP = bg.closest('p');
-    block.append(bg);
-    if (bgP) bgP.remove();
-    row.classList.add('cards-card-body');
+  if (block.children.length % 4 === 0) {
+    block.classList.add('cards-four-up');
+  } else {
+    block.classList.add('cards-three-up');
+  }
 
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    const cardLink = row.querySelector('.button-container > a');
+    if (cardLink) {
+      const cardButtonContainer = cardLink.closest('.button-container');
+
+      const cardLinkTitle = cardLink.textContent;
+      cardLink.innerHTML = '';
+      cardLink.className = 'card-link-wrapper';
+      li.append(cardLink);
+
+      [...row.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'cards-card-image';
+        } else {
+          div.className = 'cards-card-body';
+          if (div !== cardButtonContainer) cardButtonContainer.remove();
+          div.insertAdjacentHTML('beforeEnd', `<span class="card-more-cta">${cardLinkTitle}</span>`);
+        }
+
+        cardLink.append(div);
+      });
+    } else {
+      [...row.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'cards-card-image';
+        } else {
+          div.className = 'cards-card-body';
+        }
+
+        li.append(div);
+      });
+    }
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }
